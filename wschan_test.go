75a07c9025081f7aa61ef389010006917a262f2b
Alexander Glyzov <bonoba@gmail.com>
2017-07-09 19:54:41 +0300

    fixed tests after httptest.Server.CloseClientConnections() stopped working properly

--- wschan_test.go
+++ wschan_test.go
@@ -15,15 +15,22 @@ import (
 
 // --- utils ---
 
-type echoHandler testing.T
-func (t *echoHandler) ServeHTTP(ans http.ResponseWriter, req *http.Request) {
-	ws, err := websocket.Upgrade(ans, req, nil, 1024, 1024)
-	if _, ok := err.(websocket.HandshakeError); ok {
-		t.Logf("bad handshake: %v", err)
-		http.Error(ans, "Not a websocket handshake", 400)
-		return
-	} else if err != nil {
-		t.Logf("upgrade error: %v", err)
+type Echoer struct {
+	test	*testing.T
+}
+
+var upgrader = websocket.Upgrader{} // use default options
+
+func NewEchoer(t *testing.T) *Echoer {
+	return & Echoer {
+		test   : t,
+	}
+}
+
+func (e *Echoer) ServeHTTP(ans http.ResponseWriter, req *http.Request) {
+	ws, err := upgrader.Upgrade(ans, req, nil)
+	if err != nil {
+		e.test.Errorf("HTTP upgrade error: %v", err)
 		return
 	}
 	defer ws.Close()
@@ -31,12 +38,16 @@ func (t *echoHandler) ServeHTTP(ans http.ResponseWriter, req *http.Request) {
 		mt, p, err := ws.ReadMessage()
 		if err != nil {
 			if err != io.EOF {
-				t.Logf("ReadMessage: %v", err)
+				e.test.Logf("Echoer: ReadMessage error: %v", err)
 			}
 			return
 		}
+		if bytes.Equal(p, []byte("/CLOSE")) {
+			e.test.Logf("Echoer: closing connection by client request")
+			return
+		}
 		if err := ws.WriteMessage(mt, p); err != nil {
-			t.Logf("WriteMessage: %v", err)
+			e.test.Logf("Echoer: WriteMessage error: %v", err)
 			return
 		}
 	}
@@ -85,7 +96,7 @@ func TestBadURL(t *testing.T) {
 	}
 }
 func TestConnect(t *testing.T) {
-	srv := httptest.NewServer((*echoHandler)(t))
+	srv := httptest.NewServer(NewEchoer(t))
 	defer srv.Close()
 
 	URL := http_to_ws(srv.URL)
@@ -113,7 +124,7 @@ func TestConnect(t *testing.T) {
 	srv.Close()
 }
 func TestReconnect(t *testing.T) {
-	srv := httptest.NewServer((*echoHandler)(t))
+	srv := httptest.NewServer(NewEchoer(t))
 	defer srv.Close()
 
 	URL := http_to_ws(srv.URL)
@@ -121,14 +132,18 @@ func TestReconnect(t *testing.T) {
 
 	ws := New(URL, http.Header{})
 
-	<-ws.Status  // CONNECTING
-	<-ws.Status  // CONNECTED
+	if st := <-ws.Status; st.State != CONNECTING {
+		t.Errorf("st.State is %v, extected CONNECTING", st.State)
+	}
+	if st := <-ws.Status; st.State != CONNECTED {
+		t.Errorf("st.State is %v, extected CONNECTED", st.State)
+	}
 
-	// server unexpectedly closes connection
-	srv.CloseClientConnections()
+	// server unexpectedly closes our connection
+	ws.Output <- []byte("/CLOSE")
 
 	// wait for a re-connection
-	for _, state := range []byte{DISCONNECTED, CONNECTING, CONNECTED} {
+	for _, state := range []State{DISCONNECTED, CONNECTING, CONNECTED} {
 		select {
 		case st := <-ws.Status:
 			t.Log(state)
@@ -143,10 +158,9 @@ func TestReconnect(t *testing.T) {
 	// cleanup
 	ws.Command <- QUIT
 	<-ws.Status  // DISCONNECTED
-	srv.Close()
 }
 func TestServerDisappear(t *testing.T) {
-	srv := httptest.NewServer((*echoHandler)(t))
+	srv := httptest.NewServer(NewEchoer(t))
 	defer srv.Close()
 
 	URL := http_to_ws(srv.URL)
@@ -154,15 +168,20 @@ func TestServerDisappear(t *testing.T) {
 
 	ws := New(URL, http.Header{})
 
-	<-ws.Status  // CONNECTING
-	<-ws.Status  // CONNECTED
+	if st := <-ws.Status; st.State != CONNECTING {
+		t.Errorf("st.State is %v, extected CONNECTING", st.State)
+	}
+	if st := <-ws.Status; st.State != CONNECTED {
+		t.Errorf("st.State is %v, extected CONNECTED", st.State)
+	}
 
 	// server unexpectedly disappear
-	srv.CloseClientConnections()
+	ws.Output <- []byte("/CLOSE")
+	srv.Listener.Close()
 	srv.Close()
 
 	// expect a re-connection attempt and then waiting
-	for _, state := range []byte{DISCONNECTED, CONNECTING, DISCONNECTED, WAITING} {
+	for _, state := range []State{DISCONNECTED, CONNECTING, DISCONNECTED, WAITING} {
 		select {
 		case st := <-ws.Status:
 			t.Log(state)
@@ -179,7 +198,7 @@ func TestServerDisappear(t *testing.T) {
 	<-ws.Status  // DISCONNECTED
 }
 func TestEcho(t *testing.T) {
-	srv := httptest.NewServer((*echoHandler)(t))
+	srv := httptest.NewServer(NewEchoer(t))
 	defer srv.Close()
 
 	URL := http_to_ws(srv.URL)
@@ -187,12 +206,16 @@ func TestEcho(t *testing.T) {
 
 	ws := New(URL, http.Header{})
 
-	<-ws.Status  // CONNECTING
-	<-ws.Status  // CONNECTED
-
-	// send a message
+	// send a message right away
 	orig := []byte("Test Message")
 	ws.Output <- orig
+
+	if st := <-ws.Status; st.State != CONNECTING {
+		t.Errorf("st.State is %v, extected CONNECTING", st.State)
+	}
+	if st := <-ws.Status; st.State != CONNECTED {
+		t.Errorf("st.State is %v, extected CONNECTED", st.State)
+	}
 
 	// wait for an answer
 	select {
